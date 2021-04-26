5ce94f4d9960f709ef32d816a04a570ccbe6bb26
unknown <samar.mohamed.5523@gmail.com>
2021-04-25 18:32:52 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      initCount : 5
+  
+    }
+  }
+
+  setIntCount=(value)=>{
+    this.setState({initCount:value})
+  }
+
+  render() {
+    return (
+      <div>
+        <Counter2 initCount={this.state.initCount} ChangeInitCount={this.setIntCount} />
+
+      </div>
+    );
+
+
+  }
+  
 }
+
+class Counter extends React.Component 
+{
+
+  constructor(){
+    super();
+    this.state = {
+      count : 0,
+  
+    }
+  }
+
+  increase=()=>{
+    this.setState({count:this.state.count +1 });
+  }
+  decrease=()=>{
+    this.setState({count:this.state.count -1 });
+  }
+  render() {
+    return (
+      <div>
+        <div> {this.state.count}</div>
+      <button onClick={this.increase}> Increase</button>
+      <button onClick={this.decrease}> Decrease</button>
+
+      </div>
+      
+    );
+
+
+  }
+
+}
+
+class Counter2 extends React.Component 
+{
+
+  constructor(props){
+    super();
+    this.state = {
+      count :props.initCount,
+  
+    }
+  }
+
+  ChangeCount=(op)=>{
+    if(op == "+")
+    this.setState({count:this.state.count +1 });
+    else if(op == "-")
+    this.setState({count:this.state.count -1 });
+
+    this.props.ChangeInitCount(18)
+
+  }
+  
+  render() {
+    return (
+      <div>
+        <div> {this.state.count}</div>
+      <button onClick={()=>this.ChangeCount("+")}> Increase</button>
+      <button onClick={()=>this.ChangeCount("-")}> Decrease</button>
+
+      </div>
+      
+    );
+
+
+  }
+
+}
+
+
+
+/*
+class App extends React.Component {
+constructor(){
+  super();
+  this.state = {
+    name : "Ahmed",
+
+  }
+}
+
+ChangeName=()=>{
+  this.setState({name:"Mohamed"});
+}
+  
+
+
+
+  render() {
+    return (
+      <div>
+          Welcome  samar {this.state.name}
+        <button onClick={this.ChangeName}> ChangeName</button>
+
+          <Footer/>
+
+      </div>
+    );
+
+
+  }
+  
+}
+
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <div>
+          copy right 2021
+      </div>
+    );
+
+
+  }
+
+}
+
+*/
 
 export default App;
